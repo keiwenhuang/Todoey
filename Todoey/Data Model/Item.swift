3026b4391c86889bd83a35d4175640cd0d55f2bf
Kei Wen Huang <kaoghk@gmail.com>
2018-12-13 16:09:43 -0800

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Kevin Huang on 12/13/18.
+//  Copyright © 2018 Kevin Huang. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
